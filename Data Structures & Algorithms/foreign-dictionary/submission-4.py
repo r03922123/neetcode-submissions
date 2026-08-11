49b029ea6ce02917c24b58d2c3a3ee0fd86d06a1
jie hong lin <csam020410@gmail.com>
2026-08-12 01:33:16 +0800

    Add: foreign-dictionary - submission-4

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-4.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        N = len(words)
+        graph = {}
+        deg = {c: 0 for c in set(c for word in words for c in word )}
+        for i in range(N-1):
+            s1, s2 = words[i], words[i + 1]
+            m, n = len(s1), len(s2)
+            for k in range(m):
+                if k == n: return ""
+                if s1[k] != s2[k]:
+                    if s2[k] not in graph.setdefault(s1[k], set()):
+                        graph[s1[k]].add(s2[k])
+                        deg[s2[k]] += 1
+                    break
+
+        queue = deque(c for c, d in deg.items() if d == 0)
+        res = []
+        while queue:
+            u = queue.popleft()
+            res.append(u)
+            for v in graph.get(u, []):
+                deg[v] -= 1
+                if deg[v] == 0:
+                    queue.append(v)
+        
+        return "".join(res) if len(res) == len(deg) else ""
+
+    
+
+                
